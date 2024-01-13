e64845d6c60f2a890be07e3917354fb1a0a35e12
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2024-01-13 15:48:29 -0500

    Factory pattern and RORO(Recive an object, return an object)

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Object-oriented-programming-practice
+# Object-Oriented Programming Practice
+
+This repository contains practice code for Object-Oriented Programming (OOP) principles, showcasing various aspects such as classes, inheritance, encapsulation, and polymorphism in JavaScript.
+
+## Classes and Objects
+
+### Comment Class
+
+The `Comment` class represents a comment made by a student. It has properties such as `content`, `studentName`, `studentRole`, and `likes`. The `post()` method is responsible for displaying the comment information.
+
+### platziClass Class
+
+The `platziClass` class represents a Platzi class with properties like `name` and `videoID`. It has methods like `reproducir()` and `pausar()` for handling video playback.
+
+### Student Classes
+
+There are several classes representing different types of students:
+- `StudentTwo`: A basic student class with properties like `name`, `age`, and `cursosAprobados`.
+- `studentThree`: A class created with the concept of receiving an object and returning an object (ROR). It has properties like `name`, `age`, `email`, and `cursosAprobados`.
+- `freeStudent`, `basicStudent`, `expertStudent`, `teacherStudent`: Subclasses of the `Student` class with specific approval logic for courses based on the student's type.
+
+### Course Class
+
+The `Course` class represents a course with properties like `name`, `classes`, `isFree`, and `lang`. It uses getter and setter methods for the `name` property.
+
+### learningPath Class
+
+The `learningPath` class represents a learning path with properties like `escuela` and `cursos`.
+
+## Usage Examples
+
+The `readme.md` file includes usage examples for creating instances of these classes and utilizing their methods.
+
+### Example:
+```javascript
+const Swald = new freeStudent({
+    name: 'Oswaldo',
+    username: 'swaldramjf',
+    points: 52222,
+    learningPath: [escuelaData, escuelaWeb]
+})
+
+Swald.postComment('This is a great learning path!')
+Swald.approveCourse(progBasica)
+
+console.log(Swald)
---
+++ intermedioPOO/main.js
@@ -0,0 +1,285 @@
+
+const Ale ={
+    name: 'Alejandro',
+    approvedCourses: ['Curso Html'],
+    aprovarCurso(course){
+        this.approvedCourses.push(course)
+
+    }
+}
+
+console.log(Object.keys(Ale))
+console.log(Object.getOwnPropertyNames(Ale))
+console.log(Object.entries(Ale))
+
+
+
+//Atributos y metodos estaticos en JS:
+
+class burro{
+    static makeNoise(){
+        return `HOLI BURRR`
+    }
+}
+
+console.log(burro.makeNoise())
+
+/*
+
+-Object.define.property => recibe tres argumentos, el objeto, la propiedad y el atributo en {}
+-Las propiedades se definen con cuatro parametros(value, configurable, writable, enumerable)
+-value es el valor de la propiedad
+- Si configurable es false, no se puede eliminar la propiedad
+- Si writable es false, no se puede editar la propiedad
+- Si enumerable es false, no se puede listar la propiedad con Object.keys()
+
+*/ 
+
+
+const car = {
+  mark : 'Tida',
+  model : 2022,
+  color: 'blue'
+}
+
+/* Object.defineProperty(car, 'mileage', {
+    value : 20000,
+    enumerable : false,
+    writable: true,
+    configurable : true
+})
+
+Object.defineProperty(car, 'power', {
+    value: 22,
+    enumerable : true,
+    writable: false,
+    configurable : true
+})
+
+Object.defineProperty(car, 'wheels', {
+    value: 'Michellin',
+    enumerable : true,
+    writable: true,
+    configurable : false 
+})
+
+Object.defineProperty(car, 'Material', {
+    value: 'Steal',
+    enumerable : false,
+    writable: false,
+    configurable : false 
+    
+}) */
+
+Object.seal(car) // no deja eliminar las propiedades del objeto
+Object.freeze(car) // no deja eliminar ni editar las propiedades del objeto.
+console.log(Object.getOwnPropertyDescriptors(car))
+
+
+/*
+SHALLOW COPY
+-Se refiere a copiar objetos con ciclos, Object.assign y Object.create
+-sirve cuando las propiedades de los objetos son normales pero cuando hay un objeto dentro, NO.
+*/ 
+
+const obj1 = {
+    a: 'a',
+    b: 'b',
+    c:{
+        d: 'd',
+        e: 'e'
+    },
+
+    editA(){
+        this.a = 'AAAA'
+
+    }
+}
+
+const obj2 = {}
+
+for(prop in obj1){
+    obj2[prop] = obj1[prop]
+}
+
+const obj3 = Object.assign({}, obj1)
+const obj4 = Object.create(obj1)
+
+/*Json.stringify, Json.parse
+- sirve para que no se cambien las propiedades normales y las de los objetos dentro de objetos
+pero no funciona con metodos.
+*/ 
+
+const objectComplexString = JSON.stringify(obj1)
+const obj = JSON.parse(objectComplexString)
+
+console.log(objectComplexString)
+console.log(obj)
+
+/*Recursividad, es cuando un funcion se llama por si misma dentro de la misma funcion
+siempre tiene que haber algo que corte la funcion recursiva*/
+
+
+let numerito = 0
+
+while( numerito <= 5 ){
+    console.log(numerito)
+    numerito++
+}
+
+
+function recursiva(number){
+    console.log(number)
+    if(number <5){
+        return recursiva(number + 1)
+    }else{
+        return 5;
+    }
+}
+
+//Creando funciones recursivas, primero con ciclo for, luego con funcion recursiva.
+
+
+//Estructura funcion recursiva
+function recursi(param){
+    if(a){
+        //llamados recursivos
+    }else{
+        //llamados normales
+    }
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,20]
+// let number = 0;
+
+// for(let index =0 ; index < numbers.length ; index ++){
+//     number = numbers[index]
+//     console.log({index, number})
+// }
+
+//funcion recursiva
+
+function recursive(numbersArray){
+    if(numbersArray.length !=0){
+        const firstNum = numbersArray[0]
+        console.log(firstNum)
+        numbersArray.shift()
+        recursive(numbersArray)
+
+    }
+}
+
+
+/*Deep copy con recursividad*/
+
+function isObject(subject){
+    return typeof subject == 'object'
+}
+
+function isArray(subject){
+    return Array.isArray(subject)
+}
+
+function deepCopy(subject){
+    let copySubject;
+
+    const subjectisObject = isObject(subject)
+    const subjectIsArray =isArray(subject)
+
+    if(subjectIsArray){
+        copySubject = []
+    } else if(subjectisObject){
+        copySubject = {}
+    } else{
+        return copySubject;
+    }
+
+    for(key in subject){
+        const keyIsObject = isObject(subject[key])
+
+        if(keyIsObject){
+            copySubject[key] = deepCopy(subject[key])
+        }else{
+            if(subjectIsArray){
+                copySubject.push(subject[key])
+            }else{
+                copySubject[key] = subject[key]
+            }
+        }
+    }
+
+    return copySubject;
+}
+
+//ABSTRACCION CON OBJETOS LITERALES Y DEEP COPY
+
+
+// Definition of a literal object named 'studentBase' serving as a template for students.
+const studentBase = {
+    name: undefined,
+    age: undefined,
+    nationality: undefined,
+    gender: undefined,
+    hobbies: undefined
+}
+
+// Creation of a deep copy of the 'studentBase' object named 'Alejito' using the 'deepCopy' function.
+const Alejito = deepCopy(studentBase)
+
+// Seal the 'Alejito' object, preventing the addition or removal of properties but allowing the modification of existing values.
+Object.seal(Alejito)
+
+// Assign the value 'Alejo' to the 'name' property of the 'Alejito' object.
+Alejito.name = 'Alejo'
+
+// Define the 'name' property of the 'Alejito' object using Object.defineProperty.
+// A new value ('Jhon Romero') is set, and the property is configured as non-configurable (configurable: false).
+Object.defineProperty(Alejito, 'name', {
+    value: 'Jhon Romero',
+    configurable: false
+})
+
+
+
+
+// FACTORY PATTERN AND RORO => Patterns that help create object molds through functions.
+
+// Function 'requiredParam' that throws an error indicating the specified parameter is mandatory.
+function requiredParam(param){
+    throw new Error(`${param} is mandatory`);
+}
+
+// Function 'createStudent' using the Factory Pattern to create a student object.
+// Parameters are destructured from an object, providing default values, and using 'requiredParam' for mandatory parameters.
+function createStudent({
+    name = requiredParam('name'),
+    email = requiredParam('email'),
+    age,
+    approvedCourses = [],
+    facebook,
+    twitter,
+    instagram
+} = {})
+{
+    return {
+        name,
+        email,
+        age,
+        approvedCourses,
+        facebook,
+        twitter,
+        instagram
+    };
+}
+
+// Example of using 'createStudent' to create a student object named 'elJhon'.
+const elJhon = createStudent({
+    name: 'Jhon',
+    email: 'hijodelrey@hotmail.com'
+});
+
+
+
+
+
+
